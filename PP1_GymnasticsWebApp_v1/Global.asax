e746c7b19aca05f1542d7e9244d0a371d08ca646
Jennifer Evans <jenevans3ten@gmail.com>
2016-04-29 18:37:41 -0400

    Initial Commit new web project

---
+++ PP1_GymnasticsWebApp_v1/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="PP1_GymnasticsWebApp_v1.MvcApplication" Language="C#" %>
---
+++ PP1_GymnasticsWebApp_v1/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(PP1_GymnasticsWebApp_v1.Startup))]
+namespace PP1_GymnasticsWebApp_v1
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
